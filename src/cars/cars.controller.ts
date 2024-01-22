bc207115ba4c0a1b9ef8bcd0b6f44f9df0d3b03f
Ernesto Canquiz <caribestic123@gmail.com>
2024-01-22 10:39:54 -0400

    v12: Dependency injection

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,19 +1,21 @@
 import { Controller, Get, Param } from '@nestjs/common';
-
+import { CarsService } from './cars.service'
 @Controller('cars')
 export class CarsController {
 
-  private cars = ['Toyota', 'Honda', 'Jeep'];
+  constructor(
+    private readonly carsService: CarsService
+  ){}
 
   @Get()
-  getAllCars() {    
-    return this.cars;
+  getAllCars() {
+    return this.carsService.findAll();
   }
 
   @Get(':id') 
-  getCarById(@Param('id') id) { 
-    console.log({id})   
-    return this.cars[id];
+  getCarById(@Param('id') id: string) { 
+    //console.log({id})   
+    return this.carsService.findOneById(+id);
   }
 
 }
