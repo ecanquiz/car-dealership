860b7a0f7397aad4f2157529498291a2117140f7
Ernesto Canquiz <caribestic123@gmail.com>
2024-01-30 15:17:41 -0400

    v8: Global Pipes - At the Application Level

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,8 +1,9 @@
-import { IsString } from "class-validator";
+import { IsString, MinLength } from "class-validator";
 
 export class CreateCarDTO {
     @IsString({ message: `The brand most be a cool string`})
     readonly brand: string;
     @IsString()
+    @MinLength(3)
     readonly model: string;
 }
