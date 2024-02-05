575a9053349e4cb5f67a698a18bad90d966d9243
Ernesto Canquiz <caribestic123@gmail.com>
2024-02-05 14:22:28 -0400

    v10: Update a car

---
+++ src/cars/dto/update-car.dto.ts
@@ -0,0 +1,18 @@
+import { IsOptional, IsString, IsUUID, MinLength } from "class-validator";
+
+export class UpdateCarDTO {
+
+    @IsString()
+    @IsUUID()
+    @IsOptional()
+    readonly id?: string;
+
+    @IsString({ message: `The brand most be a cool string`})
+    @IsOptional()
+    readonly brand?: string;
+
+    @IsString()
+    @MinLength(3)
+    @IsOptional()
+    readonly model?: string;
+}
